f5a249d52eff8d2339d3ed9aaceadde12a7da1d0
bedirhan <bedirhantantan@outlook.com>
2023-08-22 20:14:13 +0300

    github project download

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+// using Project.Models;
+
+namespace Project.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    public IActionResult Index() {
+        return View();
+    }
+
+    public IActionResult Iletisim() {
+        return View();
+    }
+
+    public IActionResult Hizmetler() {
+        return View();
+    }
+
+    public IActionResult Hakkimizda() {
+        return View();
+    }
+   
+}
---
+++ Project/bin/Debug/net7.0/publish/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function() {
+    const faqItems = document.querySelectorAll(".faq");
+
+    faqItems.forEach(function(item) {
+        item.addEventListener("click", function() {
+            if (item.classList.contains("active")) {
+                item.classList.remove("active");
+            } else {
+                faqItems.forEach(function(faq) {
+                    faq.classList.remove("active");
+                });
+                item.classList.add("active");
+            }
+        });
+    });
+});
+
+
